956fdfcc82bab60f85a248472b00c24055252cbe
jim2935 <geldinahmed@gmail.com>
2023-07-22 01:29:43 +0100

    reverting changes that were made to solve merge conflicts

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,24 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql2');
-const app = express()
-const cors = require("cors");
-app.use(cors());app.use(express.json());
-const port = process.env.PORT || 19007
 
-DATABASE_URL='mysql://quuk2dwg7cgsy4xn3pu8:************@aws.connect.psdb.cloud/sms?ssl={"rejectUnauthorized":true}'
 
-const databaseUrl = new URL(process.env.DATABASE_URL);
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-
-dbConnection.connect(err => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database with thread number', dbConnection.threadId);
-});
 
 
 
