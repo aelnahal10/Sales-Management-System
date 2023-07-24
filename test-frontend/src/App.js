b883ba7ef46341e895c4109da21685736ca06748
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-07-24 13:34:23 +0100

    A successfull run of react for the API tests

--- test-frontend/src/App.js
+++ test-frontend/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const addUser = async () => {
+        const response = await fetch("http://172.16.3.142:3000/users/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, password }),
+        });
+
+        if (response.ok) {
+            // If the response status is 200-299, it indicates success.
+            const data = await response.text();
+            setMessage(data); // Update the message state with the text response
+        } else {
+            // If the response status is not in the range 200-299, it indicates an error.
+            setMessage("Error: Failed to add user."); // Set a custom error message
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Add User</h1>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button onClick={addUser}>Add User</button>
+            </div>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default App;
